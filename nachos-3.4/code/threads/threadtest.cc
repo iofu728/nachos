50d9f422407bf8b18e19305f8ecd93882527818e
gunjianpan <iofu728@163.com>
2019-03-01 21:34:38 +0800

    🐽 lab1 experience 4

--- nachos-3.4/code/threads/threadtest.cc
+++ nachos-3.4/code/threads/threadtest.cc
@@ -52,7 +52,8 @@ void ThreadTest1() {
 //----------lab1-Test-Begin--------------------------------------------
 
 void Lab1Thread(int someone) {
-  for (int i = 0; i <= 3; ++i) {
+  printf("Write by Jiang Huiqiang 1801210840\n");
+  for (int i = 0; i <= 6; ++i) {
     printf("threadname: %s tid: %d uid: %d  looped %d times\n",
            currentThread->getName(), currentThread->getTid(),
            currentThread->getUid(), i);
